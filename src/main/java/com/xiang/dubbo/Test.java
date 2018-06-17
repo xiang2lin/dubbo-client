f99891d612f060210ff055942a06fc2485e3de3b
xiang2lin <251481237@qq.com>
2018-06-17 22:51:01 +0800

    随机负载

--- src/main/java/com/xiang/dubbo/Test.java
+++ src/main/java/com/xiang/dubbo/Test.java
@@ -6,10 +6,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * xiang 2018/6/14
  **/
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("dubbo-client.xml");
         //得到IHelloService 远程代理对象
-        IHelloService helloService= (IHelloService) context.getBean("helloService");
-        System.out.println(helloService.sayHello("World"));
+
+        //循环调用,模拟负载均衡  默认使用随机负载
+        for (int i=0;i<10;i++){
+            IHelloService helloService= (IHelloService) context.getBean("helloService");
+            System.out.println(helloService.sayHello("World"));
+            Thread.sleep(1000);
+        }
+
     }
 }
